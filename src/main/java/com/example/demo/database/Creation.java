14de40b95292a145382cfebe44cb26509d952849
Daniel Barco López <78450705+Lamermelada42@users.noreply.github.com>
2023-03-02 11:37:19 -0500

    Adicionar integraciones de SQLite
    Se adicionan las funciones de SQLite y se importan librerias para su funconalidad

---
+++ src/main/java/com/example/demo/database/Creation.java
@@ -0,0 +1,47 @@
+package com.example.demo.database;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Creation {
+
+    public static void createNewDatabase(String fileName) {
+
+        String url = "jdbc:sqlite:D:/I.D.G/E.S/SQLite/DBGuarderia/" + fileName;
+
+        try {
+            Connection connection = DriverManager.getConnection(url);
+            if (connection != null) {
+                DatabaseMetaData meta = connection.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void createNewTable() {
+
+        String url = "jdbc:sqlite:D://I.D.G/E.S/SQLite/DBGuarderia/Dogcare.db";
+
+        String sql = "CREATE TABLE IF NOT EXISTS Clients (\n"
+                + " name text NOT NULL,\n"
+                + " document text NOT NULL PRIMARY KEY,\n"
+                + " address text NOT NULL,\n"
+                + " pet text NOT NULL,\n"
+                + ");";
+
+        try{
+            Connection connection = DriverManager.getConnection(url);
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
