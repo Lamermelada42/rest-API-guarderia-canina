abee954cae070fd9034be2e05e481b716cd63fcb
GermanG55 <grmnrtr@gmail.com>
2023-03-02 21:01:09 -0500

    "Crear Nuevo DTO de Reservas"

--- src/main/java/com/example/demo/controller/dto/ReserveDTO.java
+++ src/main/java/com/example/demo/controller/dto/ReserveDTO.java
@@ -3,14 +3,12 @@ package com.example.demo.controller.dto;
 import lombok.Setter;
 import lombok.Data;
 import lombok.Getter;
-import lombok.ToString;
 
 @Data
 public class ReserveDTO {
-    @Setter @Getter private String owner_name;
     @Setter @Getter private String document;
-    @Setter @Getter private String address;
     @Setter @Getter private String pet_name;
+    @Setter @Getter private String service_date;
     public ReserveDTO(){
 
     }
@@ -18,9 +16,8 @@ public class ReserveDTO {
     public String toStringUsuario(){
 
         return "UsuarioDTO{" +
-                "owner='" + owner_name + '\'' +
+                "service_date='" + service_date + '\'' +
                 ", document='" + document + '\'' +
-                ", address='" + address + '\'' +
                 ", pet_name=" + pet_name +
                 '}';
     }
--- src/main/java/com/example/demo/controller/dto/UserDTO.java
+++ src/main/java/com/example/demo/controller/dto/UserDTO.java
@@ -3,7 +3,6 @@ package com.example.demo.controller.dto;
 import lombok.Setter;
 import lombok.Data;
 import lombok.Getter;
-import lombok.ToString;
 
 @Data
 public class UserDTO {
